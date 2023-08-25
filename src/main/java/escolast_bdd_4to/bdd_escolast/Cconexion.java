1fc6b3015f521d9d204eeb642ec8ae0bfda758ef
adrianC05 <aacarrion4@espe.edu.ec>
2023-08-25 10:09:52 -0500

    V1

---
+++ src/main/java/escolast_bdd_4to/bdd_escolast/Cconexion.java
@@ -0,0 +1,31 @@
+package escolast_bdd_4to.bdd_escolast;
+
+//@author Adrian Carrion
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+public class  Cconexion {
+   public static Connection estblecerConexion(){
+        Connection conectar = null;
+        
+        String serverName = "ADRIANCARRION\\SQLSERVER1";
+        String usuario = "sa";
+        String contrasenia = "1982";
+        String bd = "escolast";
+        String ip = "192.168.1.2";
+        String puerto = "1433";
+
+        String cadena = "jdbc:sqlserver://" + ip + ":" + puerto + ";databaseName=" + bd + ";serverName=" + serverName;
+
+        try {
+            conectar = DriverManager.getConnection(cadena, usuario, contrasenia);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al conectar la base de datos: " + e.toString());
+        }
+        return conectar;
+        }
+}
